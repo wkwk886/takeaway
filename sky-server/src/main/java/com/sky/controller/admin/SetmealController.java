1515e67667f051c36e75a456fcbb100e5df32b07
user <hexx31313@gmail.com>
2024-12-23 00:40:06 +0800

    苍穹外卖项目-添加购物车

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +30,7 @@ public class SetmealController {
 
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key="#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         log.info("新增套餐：{}",setmealDTO);
         setmealService.saveWithDish(setmealDTO);
@@ -51,6 +53,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)//清理setmealCache下所有缓存
     public Result deleteBatch(@RequestParam List<Long> ids){
         log.info("删除套餐：{}",ids);
         setmealService.deleteBatch(ids);
@@ -72,6 +75,7 @@ public class SetmealController {
      */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)//清理setmealCache下所有缓存
     public Result update(@RequestBody SetmealDTO setmealDTO){
         setmealService.update(setmealDTO);
         return Result.success();
@@ -82,6 +86,7 @@ public class SetmealController {
      */
     @PostMapping("/status/{status}")
     @ApiOperation("修改起售停售")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)//清理setmealCache下所有缓存
     public Result startOrStop(@PathVariable Integer status,Long id){
         setmealService.startOrStop(status,id);
         return Result.success();
---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+public interface ShoppingCartService {
+
+    /*
+    添加购物车
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
